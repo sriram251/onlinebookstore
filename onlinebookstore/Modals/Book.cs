c41af1ccfabfa5e7eec18902d7c456207ac5219d
sriram251 <sriramraja251@gmail.com>
2021-04-11 09:23:29 +0530

    Add project files.

---
+++ onlinebookstore/Modals/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace onlinebookstore.Modals
+{
+    public class Book
+    {   [Key]
+        public int Books_id { get; set; }
+        public string Title { get; set; }
+        public int Auther_id { get; set; }
+        public DateTime Published_date { get; set; }
+        [Required]
+        public string shortDescription { get; set; }
+        [Required]
+        public string longDescription { get; set; }
+        public int coverImage_id { get; set; }
+        public int sampleDoc_id { get; set; }
+    }
+}
